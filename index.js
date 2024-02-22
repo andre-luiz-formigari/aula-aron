b4ee9a589e905ffad15baa32a0fb00ac046634ea
andre-luiz-formigari <132684560+andre-luiz-formigari@users.noreply.github.com>
2024-02-22 08:54:46 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var pessoa = "832.192.304-34"
+
+console.log(`conteudo Inicial: ${pessoa} | Tipo : ${typeof(pessoa)}`);
+
+pessoa = 259
+
+console.log(`mudei de conteudo: ${pessoa} | Tipo : ${typeof(pessoa)}`);
