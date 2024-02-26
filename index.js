a3fdc122e4128774c2b8c6ed2caf409cc71a9de9
andre-luiz-formigari <132684560+andre-luiz-formigari@users.noreply.github.com>
2024-02-26 11:41:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ console.log(`conteudo Inicial: ${pessoa} | Tipo : ${typeof(pessoa)}`);
 pessoa = 259
 
 console.log(`mudei de conteudo: ${pessoa} | Tipo : ${typeof(pessoa)}`);
+
+Numero = 1
+numero2 = 2
+
+
+total =Numero + numero2
+
+console.log(`total da soma e ${total} `)
+
+total =Numero - numero2
+
+console.log(`total da subtração e ${total} `)
+
+total =Numero * numero2
+
+console.log(`total da multiplicação e ${total} `)
+
+total =Numero / numero2
+
+console.log(`total da divisão e ${total} `)
